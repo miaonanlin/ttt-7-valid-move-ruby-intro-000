bf85d734fb0c6e5aa0c9dba49c84905f16f4d6ab
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-04 23:27:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
  def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
@@ -18,4 +18,3 @@ else
   return false
 end
 end
-
