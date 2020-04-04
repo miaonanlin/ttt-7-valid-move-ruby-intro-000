f21ebe1926af63cd8a0fedcb8bc8631233c2a465
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-04 23:31:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,3 +15,12 @@ def valid_move?(board, index)
       return false
     end
   end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+
+
+end
