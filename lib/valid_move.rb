4291571bb1e62444b243d9845012070af4b1951a
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-04 23:30:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,20 +1,17 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  def position_taken?(array, inde)
-  if array[inde] == " " || array[inde] == "" || array[inde] == nil
-    return false
-  else
-    return true
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
   end
-end
-
-
-
 
   def on_board?(num)
-    if num.between?(0,8) == true
+    if num.between?(0, 8) == true
       return true
     else
       return false
+    end
   end
-end
